b9845514da7fce9877b30557c61ac23f62f9fa70
Edward Kmett <ekmett@gmail.com>
2012-03-29 15:46:03 -0400

    more strongly typed bindings

--- Objective/C/Runtime.hs
+++ Objective/C/Runtime.hs
@@ -1,26 +1,37 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 {-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE DefaultSignatures #-}
+{-# LANGUAGE TypeFamilies #-}
 module Objective.C.Runtime
   (
+    Nil(..)
   -- * Classes
-    Class(..), GetClass(..)
-  , getSuperclass, isMetaClass
-  , getVersion, setVersion
+  , Class(..)
   , getInstanceSize
-  -- * Object Identities
-  , Id(..)
-  , Nil(..)
+  , getVersion
+  , setVersion
+  -- * Superclasses
+  , Super
+  , getSuperclass
+  -- * MetaClasses
+  , Meta
+  , isMetaClass
+  -- * Objects
+  , Id(..), getClassOf, getClassNameOf
   -- * Selectors
-  , Selector(..)
-  , selector
-  , selectorName
-  , selectorIsMapped
-  -- * Unsafe core
-  , HasId(..)
-  , copy
+  , Selector(..), isMapped
+  -- * Strongly Typed ClassPairs
+  , ClassPair(..)
+  -- * Arbitrary selectors
+  , Sel, sel
+  -- * Arbitrary objects
+  , Object, Class_, Meta_
+  , setClass
+  , getClassByName
+  , getMetaClassByName
+  , copyObject
   ) where
 
 import Control.Applicative
@@ -35,136 +46,143 @@ import Foreign.Ptr
 import Objective.C.Prim
 import Text.Read
 
-newtype Class = Class { classPtr :: Ptr ObjcClass } deriving (Eq,Ord,Data,Typeable)
-instance Hashable Class where
-  hashWithSalt n (Class p) = n `hashWithSalt` (fromIntegral (ptrToIntPtr p) :: Int)
-
-newtype Id = Id { foreignId :: ForeignPtr ObjcObject } deriving (Eq,Ord,Data,Typeable)
-instance Hashable Id where
-  hashWithSalt n (Id p) = hashWithSalt n (fromIntegral (ptrToIntPtr (Unsafe.unsafeForeignPtrToPtr p)) :: Int)
-
-class GetClass a where
-  getClass :: a -> IO Class
-  default getClass :: HasId a => a -> IO Class
-  getClass a = Class <$> withId a c_object_getClass
-
-  getClassName :: a -> IO String
-  default getClassName :: HasId a => a -> IO String
-  getClassName a = withId a c_object_getClassName >>= peekCString
-
-  getMetaClass :: a -> IO Class
-  default getMetaClass :: HasId a => a -> IO Class
-  getMetaClass a = getClass a >>= getClass
-
-foreign import ccall unsafe "objc/runtime.h object_getClass"     c_object_getClass     :: CId -> IO CClass
-foreign import ccall unsafe "objc/runtime.h object_getClassName" c_object_getClassName :: CId -> IO CString
-
-instance GetClass Id
-instance GetClass Class
-instance GetClass String where
-  getClass name = Class <$> withCString name c_objc_getClass
-  getClassName = return
-  getMetaClass name = Class <$> withCString name c_objc_getMetaClass
-foreign import ccall unsafe "objc/runtime.h objc_getClass"     c_objc_getClass     :: CString -> IO CClass
-foreign import ccall unsafe "objc/runtime.h objc_getMetaClass" c_objc_getMetaClass :: CString -> IO CClass
-
-class GetClass a => SetClass a where
-  setClass :: a -> Class -> IO Class
-  default setClass :: HasId a => a -> Class -> IO Class
-  setClass a (Class c) = withId a $ \p -> Class <$> c_object_setClass p c
-foreign import ccall unsafe "objc/runtime.h object_setClass"     c_object_setClass     :: CId -> CClass -> IO CClass
-
-instance SetClass Class
-instance SetClass Id
-
-class GetClass a => HasId a where
-  withId :: a -> (CId -> IO b) -> IO b
-
-instance HasId Class where
-  withId (Class p) f = f (castPtr p)
-
-instance HasId Id where
-  withId (Id fp) = withForeignPtr fp
-
+-- | Nilable types
 class Nil a where
   nil   :: a
-  isNil :: a -> Bool
 
-instance Nil Id where
-  nil = nilId
-  isNil (Id fp) = Unsafe.unsafeForeignPtrToPtr fp == nullPtr
+-- | Classes
+newtype Class a = Class { cclass :: CClass } deriving (Eq,Ord,Typeable,Data)
 
-nilId :: Id
-nilId = Unsafe.unsafePerformIO $ Id <$> newForeignPtr_ nullPtr
-{-# NOINLINE nilId #-}
+instance Hashable (Class a) where
+  hashWithSalt n (Class p) = n `hashWithSalt` (fromIntegral (ptrToIntPtr p) :: Int)
 
-instance Nil Class where
+instance Nil (Class a) where
   nil = Class nullPtr
-  isNil (Class p) = p == nullPtr
 
-getSuperclass :: Class -> IO Class
-getSuperclass (Class c) = Class <$> c_class_getSuperclass c
-foreign import ccall unsafe "objc/runtime.h class_getSuperclass" c_class_getSuperclass :: CClass -> IO CClass
-
-isMetaClass :: Class -> IO Bool
-isMetaClass (Class c) = toBool <$> c_class_isMetaClass c
-foreign import ccall unsafe "objc/runtime.h class_isMetaClass" c_class_isMetaClass :: CClass -> IO CBool
-
-getInstanceSize :: Class -> IO CSize
+getInstanceSize :: Class a -> IO CSize
 getInstanceSize (Class c) = c_class_getInstanceSize c
 foreign import ccall unsafe "objc/runtime.h class_getInstanceSize" c_class_getInstanceSize :: CClass -> IO CSize
 
-getVersion :: Class -> IO Int
+getVersion :: Class a -> IO Int
 getVersion (Class c) = fromIntegral <$> c_class_getVersion c
-foreign import ccall unsafe "objc/runtime.h class_getVersion"      c_class_getVersion :: CClass -> IO CInt
+foreign import ccall unsafe "objc/runtime.h class_getVersion" c_class_getVersion :: CClass -> IO CInt
 
-setVersion :: Class -> Int -> IO ()
+setVersion :: Class a -> Int -> IO ()
 setVersion (Class c) i = c_class_setVersion c (fromIntegral i)
 foreign import ccall unsafe "objc/runtime.h class_setVersion" c_class_setVersion :: CClass -> CInt -> IO ()
 
--- unsafe bits:
-copy :: Id -> CSize -> IO Id
-copy a s = withId a $ \p -> do
-  p' <- c_object_copy p s
-  fp' <- newForeignPtr_ p'
-  return $ Id fp'
+type Meta a = Class (Class a)
+isMetaClass :: Class a -> Bool
+isMetaClass (Class c) = toBool (c_class_isMetaClass c)
+foreign import ccall unsafe "objc/runtime.h class_isMetaClass" c_class_isMetaClass :: CClass -> CBool
 
-foreign import ccall unsafe "objc/runtime.h object_copy" c_object_copy :: CId -> CSize -> IO CId
+type family Super a
+getSuperclass :: Class a -> IO (Class (Super a))
+getSuperclass (Class c) = Class <$> c_class_getSuperclass c
+foreign import ccall unsafe "objc/runtime.h class_getSuperclass" c_class_getSuperclass :: CClass -> IO CClass
 
--- nothing so far has needed a single selector
+-- | Representations of foreign objects
+class Nil a => Id a where
+  withId :: a -> (Ptr ObjcObject -> IO b) -> IO b
+  isNil  :: a -> Bool
 
-foreign import ccall unsafe "objc/objc.h sel_getName"      c_sel_getName      :: CSel -> CString
+instance Id (Class a) where
+  withId (Class cp) f = f (castPtr cp)
+  isNil (Class cp) = cp == nullPtr
+
+getClassOf :: Id a => a -> IO (Class a)
+getClassOf a = Class <$> withId a c_object_getClass
+foreign import ccall unsafe "objc/runtime.h object_getClass"     c_object_getClass     :: CId -> IO CClass
+
+getClassNameOf :: Id a => a -> IO String
+getClassNameOf a = withId a c_object_getClassName >>= peekCString
+foreign import ccall unsafe "objc/runtime.h object_getClassName" c_object_getClassName :: CId -> IO CString
+
+-- Selectors
+class Selector a where
+  selectorName :: a -> String
+  csel         :: a -> CSel
+
+isMapped :: Selector a => a -> IO Bool
+isMapped s = toBool <$> c_sel_isMapped (csel s)
+foreign import ccall unsafe "objc/objc.h sel_isMapped" c_sel_isMapped :: CSel -> IO CBool
+
+instance Selector String where
+  selectorName = id
+  csel n = Unsafe.unsafePerformIO $ withCString n c_sel_registerName
 foreign import ccall unsafe "objc/objc.h sel_registerName" c_sel_registerName :: CString -> IO CSel
 
-newtype Selector = Selector (Ptr ObjcSelector) deriving (Eq,Ord,Typeable,Data)
+-- An Unknown Selector
+newtype Sel = Sel CSel deriving (Eq,Ord,Typeable,Data)
 
-instance Nil Selector where
-  nil = Selector nullPtr
-  isNil (Selector p) = p == nullPtr
+instance Selector Sel where
+  selectorName (Sel s) = Unsafe.unsafePerformIO $ peekCString $ c_sel_getName s
+  csel (Sel p) = p
+foreign import ccall unsafe "objc/objc.h sel_getName"      c_sel_getName      :: CSel -> CString
 
--- foreign import ccall unsafe "objc/objc.h sel_isEqual" c_sel_isEqual :: CSel -> CSel -> CBool
--- instance Eq Sel where Sel s == Sel t = toBool (c_sel_isEqual s t)
--- instance Ord Sel where compare = compare `on` selectorName
+instance Show Sel where
+  showsPrec d s = showParen (d > 10) $ showString "sel " . showsPrec 10 (selectorName s)
 
-instance Hashable Selector where
-  hashWithSalt n (Selector p) = n `hashWithSalt` (fromIntegral (ptrToIntPtr p) :: Int)
-
-instance Show Selector where
-  showsPrec d s = showParen (d > 10) $ showString "selector " . showsPrec 10 (selectorName s)
-
-instance Read Selector where
+instance Read Sel where
   readPrec = parens $ prec 10 $ do
-    Ident "selector" <- lexP
-    s <- readPrec
-    return $! selector s
+    Ident "sel" <- lexP
+    (s :: String) <- readPrec
+    return $! sel s
 
--- | Find a selector by name, @selector "foo:"@ is analogous to @\@selector(foo:)@ in Objective C.
-selector :: String -> Selector
-selector s = Unsafe.unsafePerformIO $ Selector <$> withCString s c_sel_registerName
+sel :: Selector a => a -> Sel
+sel a = Sel (csel a)
 
-selectorName :: Selector -> String
-selectorName (Selector s) = Unsafe.unsafePerformIO $ peekCString $ c_sel_getName s
 
-selectorIsMapped :: Selector -> IO Bool
-selectorIsMapped (Selector s) = toBool <$> c_sel_isMapped s
-foreign import ccall unsafe "objc/objc.h sel_isMapped"     c_sel_isMapped     :: CSel -> IO CBool
+class ClassPair a where
+  getClassNameByProxy :: p a -> String
+
+  getClass :: Class a
+  getClass = r where
+    r = Unsafe.unsafePerformIO $ Class <$> withCString (getClassNameByProxy r) c_objc_getClass
+
+{-
+  getMetaClass :: Class (Class a)
+  getMetaClass = r where
+    r = Unsafe.unsafePerformIO $ Class <$> withCString (getClassNameByProxy (argOf r)) c_objc_getMetaClass
+    argOf :: a -> f a -> a
+    argOf a _ = a
+-}
+foreign import ccall unsafe "objc/runtime.h objc_getClass" c_objc_getClass :: CString -> IO CClass
+foreign import ccall unsafe "objc/runtime.h objc_getMetaClass" c_objc_getMetaClass :: CString -> IO CClass
+
+-- Objects with unknown classes
+newtype Object = Object (ForeignPtr ObjcObject) deriving (Eq,Ord,Show,Data,Typeable)
+
+instance Hashable Object where
+  hashWithSalt n (Object fp) = hashWithSalt n (fromIntegral (ptrToIntPtr (Unsafe.unsafeForeignPtrToPtr fp)) :: Int)
+
+instance Nil Object where
+  nil = nilObject
+
+nilObject :: Object
+nilObject = Unsafe.unsafePerformIO $ Object <$> newForeignPtr_ nullPtr
+{-# NOINLINE nilObject #-}
+
+instance Id Object where
+  withId (Object fp) = withForeignPtr fp
+  isNil (Object fp) = Unsafe.unsafeForeignPtrToPtr fp == nullPtr
+
+type Class_ = Class Object
+type Meta_ = Meta Object
+
+getClassByName :: String -> IO (Class Object)
+getClassByName name = Class <$> withCString name c_objc_getClass
+
+getMetaClassByName :: String -> IO (Meta Object)
+getMetaClassByName name = Class <$> withCString name c_objc_getMetaClass
+
+setClass :: Object -> Class a -> IO (Class a)
+setClass a (Class c) = withId a $ \p -> Class <$> c_object_setClass p c
+foreign import ccall unsafe "objc/runtime.h object_setClass" c_object_setClass :: CId -> CClass -> IO CClass
+
+copyObject :: Id a => a -> CSize -> IO Object
+copyObject a s = withId a $ \p -> do
+  p' <- c_object_copy p s
+  fp' <- newForeignPtr_ p'
+  return $ Object fp'
+foreign import ccall unsafe "objc/runtime.h object_copy" c_object_copy :: CId -> CSize -> IO CId
